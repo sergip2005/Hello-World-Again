a939c5e5a5d0c735f4a188e7762c33472bf63381
Dmitry Evseev <kradmiy@gmail.com>
2011-10-12 21:11:16 +0300

    architecture changes

---
+++ application/views/layout/admin/partials/user_menu.php
@@ -0,0 +1,1 @@
+<ul>	<li><a href="/login">login</a></li>	<li><a href="/logout">logout</a></li>	<li><a href="/register">register</a></li>	<li><a href="/change_password">change_password</a></li>	<li><a href="/forgot_password">forgot_password</a></li>	<li><a href="/reset_password">reset_password</a></li>	<li><a href="/activate">activate</a></li></ul>
