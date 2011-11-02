de548338259f1878d506480bd1c5d58a8456f7b2
1enny <mjkgod7@gmail.con>
2011-11-03 01:00:18 +0200

    #17

--- assets/js/site/phones.js
+++ assets/js/site/phones.js
@@ -1,22 +1,50 @@
 $(document).ready(function(){
-	$('.s').click(function() {
-		$('.cabinet').show();
-		$('.solder').hide();
-		$('.c').removeClass("selected");
-		$('.s').addClass("selected");
-		return false;
-	});
-	$('.c').click(function() {
-		$('.solder').show();
-		$('.cabinet').hide();
-		$('.s').removeClass("selected");
-		$('.c').addClass("selected");
-		return false;
-	});
+	var url = window.location.href;
+	var param = url.substring(url.lastIndexOf('/') + 1);
+	var itwas = 0;
+		$('#cabinet_img, #solder_img').jqzoom({zoomType: 'drag'});
 
-	$('#cabinet_img, #solder_img').jqzoom({zoomType: 'drag'});
+	showPartType = function (part) {
+		if(part == '#c') {
+			$('.cabinet').show();
+			$('.solder').hide();
+			$('.c').removeClass("selected");
+			$('.s').addClass("selected");
+		}
+		if(part == '#s') {
+			$('.solder').show();
+			$('.cabinet').hide();
+			$('.s').removeClass("selected");
+			$('.c').addClass("selected");
+		}
+	}
 
 	// prepare layout, fade in
 	$('.solder').hide();
 	$('div.full-transparent').delay(500).animate({opacity: 1}, 500);
+	
+	showPartType(param);
+
+	$('.s').click(function() {
+		showPartType('#c');
+		if(itwas == 0) {
+			$("#showparts").attr("href", $("#showparts").attr("href") + '/#c');
+		} else {
+			$("#showparts")
+			.attr("href", $("#showparts").attr("href").substring(0, $("#showparts").attr("href").length - 1) + 'c')
+		}
+		itwas = 1;
+		return false;
+	});
+	$('.c').click(function() {
+		showPartType('#s');
+		if(itwas == 0) {
+			$("#showparts").attr("href", $("#showparts").attr("href") + '/#s');
+		} else {
+			$("#showparts")
+			.attr("href", $("#showparts").attr("href").substring(0, $("#showparts").attr("href").length - 1) + 's');
+		}
+		itwas = 1;
+		return false;
+	});
 });
