87223c88e09d3dc2df159824434cf4c8b5061eb0
1enny <mjkgod7@gmail.con>
2011-11-23 21:52:02 +0200

    #21

--- application/views/layout/default/partials/top_menu.php
+++ application/views/layout/default/partials/top_menu.php
@@ -5,11 +5,12 @@
 </ul>
 <form name="search_form">
 	<div class="select-and-input">
-		<select name="parameter">
-			<option value="code">Код</option>
-			<option value="model">Модель</option>
-			<option value="part_name">Название детали</option>
-		</select>
+		<span value="code">Код</span>
+		<ul>
+			<li name="code">Код</li>
+			<li name="model">Модель</li>
+			<li name="part_name">Название</li>
+		</ul>
 		<input class="search_field" type="text" name="query"/>
 		<div class="search_submit"></div>
 		<input class="search_submit"  type="submit" value="Sub"/>
