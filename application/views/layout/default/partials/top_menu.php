bcb22f13d9cf38d5c8985b6728800f667a7ce2c0
1enny <mjkgod7@gmail.con>
2011-11-23 23:04:06 +0200

    #21

--- application/views/layout/default/partials/top_menu.php
+++ application/views/layout/default/partials/top_menu.php
@@ -5,14 +5,24 @@
 </ul>
 <form name="search_form">
 	<div class="select-and-input">
+		<!--
 		<span value="code">Код</span>
 		<ul>
 			<li name="code">Код</li>
 			<li name="model">Модель</li>
 			<li name="part_name">Название</li>
 		</ul>
-		<input class="search_field" type="text" name="query"/>
-		<div class="search_submit"></div>
-		<input class="search_submit"  type="submit" value="Sub"/>
+		-->
+		<input class="search_field1" type="text" name="query" value="Код"/>
+		<div class="search_submit1"></div>
+		<input class="search_submit1"  type="submit" value="Sub"/>
+
+		<input class="search_field2" type="text" name="query" value="Модель"/>
+		<div class="search_submit2"></div>
+		<input class="search_submit2"  type="submit" value="Sub"/>
+
+		<input class="search_field3" type="text" name="query" value="Название"/>
+		<div class="search_submit3"></div>
+		<input class="search_submit3"  type="submit" value="Sub"/>
 	</div>
 </form>
--- assets/js/site/script.js
+++ assets/js/site/script.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
+	/*
 // search
 	$("[name=search_form]").submit( function (e) {
+
 		e.stopPropagation();
 		e.preventDefault();
+
 		var q = $("input.search_field").val();
-		var p = $("[name=parameter]").val();
+		var p = $(".select-and-input span").attr('value');
 		if(q.length > 0) {
 			switch (p) {
 				case 'code' :
@@ -17,6 +20,58 @@ $(document).ready(function () {
 					window.location.href = '/parts/search/' + encodeURI(q);
 				break;
 			}
+		}
+	});
+
+	$(".select-and-input").delegate('span', 'click', function (e) {
+		$(".select-and-input ul").show();
+	});
+
+	$(".select-and-input").delegate('ul li', 'click', function (e) {
+		$(".select-and-input span").attr('value', $(this).attr('name'));
+		$(".select-and-input span").text($(this).text());
+		$(".select-and-input ul").hide();
+	});*/
+
+	$(".select-and-input").delegate('.search_field1', 'click', function (e) {
+		$(this).val('');
+		$(".select-and-input input.search_field2").val('Модель');
+		$(".select-and-input input.search_field3").val('Название') ;
+	});
+	$(".select-and-input").delegate('.search_submit1', 'click', function (e) {
+		var q = $(".select-and-input input.search_field1").val();
+		e.stopPropagation();
+		e.preventDefault();
+		if(q.length > 0 && q!== "Код") {
+			window.location.href = '/parts/' + encodeURI(q);
+		}
+	});
+
+	$(".select-and-input").delegate('.search_field2', 'click', function (e) {
+		$(this).val('');
+		$(".select-and-input input.search_field1").val('Код');
+		$(".select-and-input input.search_field3").val('Название');
+	});
+	$(".select-and-input").delegate('.search_submit2', 'click', function (e) {
+		var q = $(".select-and-input input.search_field2").val();
+		e.stopPropagation();
+		e.preventDefault();
+		if(q.length > 0 && q!== "Модель") {
+			window.location.href = '/parts/models/' + encodeURI(q);
+		}
+	});
+	
+	$(".select-and-input").delegate('.search_field3', 'click', function (e) {
+		$(this).val('');
+		$(".select-and-input input.search_field2").val('Модель');
+		$(".select-and-input input.search_field1").val('Код');
+	});
+	$(".select-and-input").delegate('.search_submit3', 'click', function (e) {
+		var q = $(".select-and-input input.search_field3").val();
+		e.stopPropagation();
+		e.preventDefault();
+		if(q.length > 0 && q!== "Название") {
+			window.location.href = '/parts/search/' + encodeURI(q);
 		}
 	});
 });
