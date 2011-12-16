729b8aa3c2915d4f8c2ac8d5614e89e7ec8a4794
Dmitry Evseev <kradmiy@gmail.com>
2011-12-16 16:31:17 +0200

    #27

--- application/views/layout/default/partials/search.php
+++ application/views/layout/default/partials/search.php
@@ -2,7 +2,7 @@
 	<div class="select-and-input">
 		<form name="search_parts_code">
 			<div class="search_field search_code">
-				<input class="text" placeholder="Парт-номер (6300314)" type="text" name="parts_code" value="<?php echo $search_params['parameter'] == 'part_code' ? $search_params['query'] : '' ?>">
+				<input class="text" placeholder="Парт-номер (6300314)" type="text" name="parts_code" value="<?php echo isset($search_params) ? ($search_params['parameter'] == 'part_code' ? $search_params['query'] : '') : '' ?>">
 				<div class="button"></div>
 				<input class="submit" type="submit" value="Sub">
 				<input class="parameter" type="hidden" value="parts_code">
@@ -13,7 +13,7 @@
 	<div class="select-and-input">
 		<form name="search_model_name">
 			<div class="search_field search_model">
-				<input class="text" type="text" placeholder="Модель (N8-00 RM-596)" name="model_name" value="<?php echo $search_params['parameter'] == 'models' ? $search_params['query'] : '' ?>">
+				<input class="text" type="text" placeholder="Модель (N8-00 RM-596)" name="model_name" value="<?php echo isset($search_params) ? ($search_params['parameter'] == 'models' ? $search_params['query'] : '') : '' ?>">
 				<div class="button"></div>
 				<input class="submit" type="submit" value="Sub">
 				<input class="parameter" type="hidden" value="model_name">
@@ -24,7 +24,7 @@
 	<div class="select-and-input">
 		<form name="search_parts_name">
 		<div class="search_field search_name">
-				<input class="text" type="text" placeholder="Название детали (Window Module)" name="parts_name" value="<?php echo $search_params['parameter'] == 'part_name' ? $search_params['query'] : '' ?>">
+				<input class="text" type="text" placeholder="Название детали (Window Module)" name="parts_name" value="<?php echo isset($search_params) ? ($search_params['parameter'] == 'part_name' ? $search_params['query'] : '') : '' ?>">
 				<div class="button"></div>
 				<input class="submit" type="submit" value="Sub">
 				<input class="parameter" type="hidden" value="parts_name">
