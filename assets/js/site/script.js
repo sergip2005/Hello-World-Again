2c1476585ea7ba7b91e8280448ad510d108f0265
Dmitry Evseev <kradmiy@gmail.com>
2012-01-17 18:40:05 +0200

    added app.debug property

--- assets/js/site/script.js
+++ assets/js/site/script.js
@@ -1,3 +1,139 @@
+/** Main application settings object
+ */
+var app = {
+	settings: {
+		cacheTime: 600000,//10 * 1000 * 60
+		itemCacheTime: 60000,//1000 * 60
+		spl_cacheTime: 60000,
+		prices_rates: {
+			price1: 70,
+			price2: 65,
+			price3: 60,
+			price4: 50
+		}
+	},
+
+	debug: false,
+
+	icons: {
+		close: '<img src="/assets/images/icons/close.png" class="close" title="Закрыть" />',
+		noimage: '<img src="/assets/images/icons/noimage.gif" title="Нет изображения" />'
+	},
+
+	messages: {
+		noData: 'Нет информации'
+	},
+
+	text: {
+		loading: 'Идет загрузка'
+	},
+
+	cache: {
+		vendors: {},
+		regions: {},
+		models: {}
+	},
+	/** popup */
+	splash: null,
+	popup: null,
+	popupContent: null,
+	/**
+	 * @param object s
+	 * s.w - width
+	 * s.h - height
+	 * s.html - content
+	 * s.c - callback
+	 */
+	showPopup: function (s) { // makes popup to show
+		var c = this.popupContent, p = this.popup, total_w = $(window).width(),
+			w = typeof s.w === 'undefined' || s.w <= 0 ? 'auto' : s.w,
+			h = typeof s.h === 'undefined' || s.h <= 0 ? 'auto' : s.h;
+		// resize box
+		c.html(s.html).width(w).height(h);
+		// show popup
+		this.splash.add(p).show();
+		// position popup
+		p.css('left', Math.round((total_w - p.width()) / 2) + 'px');
+		// fire callback if provided
+		if (_.isFunction(s.c)) {
+			var clb = s.c;
+			clb = _.bind(clb, c);// this reffers to #popupContent
+			clb();
+		}
+	},
+
+	/** message */
+	message: null,
+	messageContent: null,
+	/**
+	 * @param s - object with structure {html:string, c: function}
+	 */
+	showMessage: function (s) {
+		var c = this.messageContent, p = this.message;
+		c.html(s.html);
+		p.show();
+		// fire callback if provided
+		if (_.isFunction(s.c)) {
+			var clb = s.c;
+			clb = _.bind(clb, c);// this reffers to #messageContent
+			clb();
+		}
+	},
+
+	log: function(){
+		if (this.debug !== true) return false;
+		if (typeof console.log !== undefined) {
+			console.log(arguments);
+		} else {
+			return false;
+		}
+	},
+
+	initMessages: function(){
+		this.message = $('#message');
+		this.messageContent = $('#message-content');
+
+		this.popup = $('#popup');
+		this.popupContent = $('#popup-content');
+		this.splash = $('#splash').splash();
+
+		var ap = this;
+		ap.message.contents('img').click(function(){
+			ap.message.fadeOut(250);
+		});
+		ap.popup.contents('img').click(function(){
+			ap.popup.add(ap.splash).fadeOut(250);
+		});
+	},
+
+	/** shows "loading.." content in container waiting for data */
+	showLoading: function(elm){
+		$(elm).html(this.text.loading);
+	},
+
+	initSlideable: function(){
+		$('.slide-trigger').click(function(e){
+			e.preventDefault();
+			$('#' + this.id + '-content').stop().slideToggle('fast');
+		});
+	},
+
+	/** @TODO INIT */
+	init: function(){
+		this.initMessages();
+		//this.initSlideable();
+
+		$('body').ajaxStart(function() {
+				$(this).addClass('loading');
+		}).ajaxStop(function(){
+				$(this).removeClass('loading');
+			});
+	},
+
+	templates: {
+	}
+};
+
 $(document).ready(function () {
 	var search = $(".select-and-input");
 
